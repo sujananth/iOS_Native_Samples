e8519d74a05a8965d7463fd1066777f1225c7564
Sujan <sujananth_visvaratnam@comcast.com>
2020-07-12 12:17:17 +0530

    Removed NotificationCenter test, as I moved it to Unconventional Dev repository.

--- NotificationListenerTest/NotificationListenerTest/InitialViewController.swift
+++
@@ -1,8 +0,0 @@
-
-import UIKit
-//Note: You could find many duplicate codes in thi sample. This is created just to play around without affecting other classes.
-// This is just Landing View Controller to navigate.
-
-class InitialViewController: UIViewController {
-    
-}
--- NotificationListenerTest/NotificationListenerTest/ParentChildConflicts/ChildViewController.swift
+++
@@ -1,7 +0,0 @@
-
-
-import UIKit
-
-class ChildViewController: UIViewController {
-
-}
